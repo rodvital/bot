949b7ea9f59638facc9757bd41b71fcd5d2d5c25
Rodrigo Vital <rodrigo.vital@gmail.com>
2021-10-16 19:01:30 -0400

    Fazendo acesso no sistema movidesk

--- bot.py
+++ bot.py
@@ -3,15 +3,51 @@ from pyautogui import click, moveTo, typewrite
 from time import sleep
 
 
-def Localizar_Click_Imagem(imagem, tempoPos):
+def Localizar_Click_Imagem(imagem, tempoPos, alterarvertical=0, alterarhorizontal=0):
     posicao = pyautogui.locateCenterOnScreen(imagem, confidence=0.9)
     moveTo(posicao)
+
+    if alterarvertical != 0 or alterarhorizontal != 0:
+        sleep(1)
+        # print(str(posicao.x) + str(posicao.y))
+        pyautogui.move(alterarhorizontal, alterarvertical)
+        # posicao = pyautogui.position()
+        # print(str(posicao.x) + str(posicao.y))
+
     sleep(0.5)
     click()
     sleep(tempoPos)
 
 
-def Preencher_Texto_Localizar_Imagem(imagem, tempoPos, texto_preencher):
-    Localizar_Click_Imagem(imagem, tempoPos)
+def ClickMesmoLugar(tempoPos):
+    click()
+    sleep(tempoPos)
+
+
+def Preencher_Texto_Localizar_Imagem(imagem, tempoPos, texto_preencher, alterarvertical=0, alterarhorizontal=0):
+    Localizar_Click_Imagem(
+        imagem, tempoPos, alterarvertical=alterarvertical, alterarhorizontal=alterarhorizontal)
     sleep(0.5)
     typewrite(texto_preencher)
+
+
+def pressionar_tecla(tecla, tempoAntes=0, tempoPos=0):
+    if tempoAntes > 0:
+        sleep(tempoAntes)
+    pyautogui.press(tecla)
+    if tempoPos > 0:
+        sleep(tempoPos)
+
+
+def Preencher_Texto(texto_preencher, tempoPos):
+    typewrite(texto_preencher)
+    sleep(tempoPos)
+
+
+def pressionar_tecla_atalho(teclacontrole, letra, tempoAntes=0, tempoPos=0):
+    if tempoAntes > 0:
+        sleep(tempoAntes)
+    with pyautogui.hold(teclacontrole):
+        pyautogui.press(letra)
+    if tempoPos > 0:
+        sleep(tempoPos)
